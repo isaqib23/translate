667f6bcdbad11b15ed6f69a82b21cd5858e639a4
Saqib Rao <r.iqbal@oxinus.io>
2023-12-11 23:35:43 +0400

    fdsfdf

--- resources/views/frontend/success.blade.php
+++ resources/views/frontend/success.blade.php
@@ -13,7 +13,7 @@
 <div class="row featurette">
     <div class="col-md-4" style="text-align: center">
         <div id="timer" class="alert alert-primary mt-3" role="alert">
-            119
+            02:00
         </div>
         <div id="payment" class="d-none">
             <div class="alert alert-success mt-3" role="alert">
