bb577926b9d166e4e1ad3e38f657b623da571b51
Saqib Rao <r.iqbal@oxinus.io>
2024-01-08 23:33:37 +0400

    sdsd

--- resources/views/frontend/layout/footer.blade.php
+++ resources/views/frontend/layout/footer.blade.php
@@ -1,3 +1,9 @@
+<img style="display: inline-block; width: 120px" src="{{ asset('frontend/img/user_guide.jpeg') }}" />
+
+<button type="button" href="https://scribehow.com/shared/How_to_Submit_a_Translation_Order_and_Make_a_Payment__rtS9iGpYSzOq9aK7c881Cw" class="btn btn-link">How to create translating order</button>
+<button type="button" href="https://scribehow.com/shared/How_to_Submit_a_Drafting_order_on_Translingu__vV8k9U7rTAGMmVG-OdC3Mw" class="btn btn-link">How to create drafting order</button>
+<button type="button" href="https://scribehow.com/shared/How_to_Submit_a_Notary_order_on_Translingu__LaBQy3suTKWB9-ENfglXuA" class="btn btn-link">How to create notary order</button>
+<hr>
 <footer class="container">
     <p class="float-end"><a href="#">Back to top</a></p>
     <p>&copy; <?=date('Y')?> Company, Inc. &middot;
