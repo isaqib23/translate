dc5b77240e7c378de9560f808a3ecc4b106de77a
Saqib Rao <r.iqbal@oxinus.io>
2024-02-09 19:28:07 +0400

    dfd

---
+++ app/Http/Middleware/NormalizeRequestUri.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class NormalizeRequestUri
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $uri = $request->getRequestUri();
+            if (strtolower($uri) !== $uri) {
+                return redirect(strtolower($uri), 404);
+            }
+
+            return $next($request);
+    }
+}
